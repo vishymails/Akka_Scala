a8ac1f8c86fbf7e67faa423d48d7264572dbd937
vishymails <vishymails@gmail.com>
2019-05-16 15:45:22 +0530

    Add files via upload

---
+++ MonitoringPattern.scala
@@ -0,0 +1,48 @@
+package akka.first
+
+import akka.actor.{ ActorRef, ActorSystem, Props, Actor, Terminated }
+
+
+class Ares(athena: ActorRef) extends Actor {
+
+  override def preStart() = {
+    context.watch(athena)
+  }
+
+  override def postStop() = {
+    println("Ares postStop...")
+  }
+
+  def receive = {
+    case Terminated => 
+      context.stop(self)
+  }
+}
+
+class Athena extends Actor {
+
+  def receive = {
+    case msg => 
+      println(s"Athena received ${msg}")
+      context.stop(self)
+  }
+
+}
+
+object Monitoring extends App {
+
+  // Create the 'monitoring' actor system
+  val system = ActorSystem("monitoring")
+
+  val athena = system.actorOf(Props[Athena], "athena")
+
+  val ares = system.actorOf(Props(classOf[Ares], athena), "ares")
+
+  athena ! "Hi"
+  
+  athena ! "vishwanath rao"
+
+ // system.terminate();
+
+
+}
---
+++ Worker.scala
@@ -0,0 +1,16 @@
+package akka.third
+
+import akka.actor.Actor
+
+class Worker extends Actor {
+  import Worker._
+
+  def receive = {
+    case msg: Work =>
+      println(s"I received Work Message and My ActorRef: ${self}")
+  }
+}
+
+object Worker {
+  case class Work() 
+}
